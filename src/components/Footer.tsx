a62c1a65cae834e7034db8042bffd0f1e7c5c1c8
PETRANZAN Francesco <francesco.petranzan@soprasteria.com>
2024-08-14 19:28:32 +0200

    created footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import LanguageSelector from "./LanguageSelector";
+import ThemeToggle from "./ThemeToggle";
+
+const Footer = () => {
+  return (<>
+        <div className="my-20">fpetranzan.com - My personal Portfolio © 2024 Francesco Petranzan.</div>
+    </>
+  );
+};
+
+export default Footer;
