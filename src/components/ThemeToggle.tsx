a8cdfa2820ec1768b4f0dd22ee8faea665d1b442
fpetranzan <francesco.petranzan@gmail.com>
2024-08-15 11:46:31 +0200

    refactoring theme toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,13 +1,21 @@
 "use client";
-import {useTheme} from 'next-themes'
+import { useTheme } from 'next-themes'
+import {useEffect, useState} from "react"
+import { LuSun, LuMoon } from "react-icons/lu";
 
 const DarkModeToggle = () => {
   const { theme, setTheme } = useTheme();
-  return (
-    <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className='ml-auto'>
-        toggle
+  const [hasMounted, setHasMounted] = useState(false)
+
+  useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  return (<>
+    <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className='ml-auto text-lg'>
+      {hasMounted && theme === 'dark' ? <LuSun /> : <LuMoon />}
     </button>
-  );
+  </>);
 };
 
 export default DarkModeToggle;
