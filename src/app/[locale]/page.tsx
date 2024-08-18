099f9deac5cb8dd75b3c602a7441b4383234f7a6
fpetranzan <francesco.petranzan@gmail.com>
2024-08-18 12:08:08 +0200

    print new localized text on page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,17 @@
 'use client';
+
 import {useTranslations} from 'next-intl';
 
 async function HomePage() {
   const t = useTranslations('AboutMe');
+  const keys = ['one', 'two', 'three', 'four', 'five'] as const;
 
   return (<>
     <div>
       <p className="text-2xl mb-5">{t('title')}</p>
-      <p>{t('body')}</p>
+      {keys.map((key) => (
+        <p className="my-4">{t(`body.paragrapths.${key}`)}</p>
+      ))}
     </div>
   </>);
 }
