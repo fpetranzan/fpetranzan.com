179beb21c15b6a657d5405ef6d74f7c469bda7f1
fpetranzan <francesco.petranzan@gmail.com>
2024-08-18 12:05:36 +0200

    created social component

---
+++ src/components/Social.tsx
@@ -0,0 +1,25 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+
+const Social = () => {
+  
+  return (<>
+        <div className="my-10 flex gap-4">
+          <a href="https://www.linkedin.com/in/francesco-petranzan/" target="_blank">
+            <div className="flex items-center gap-1 border border-slate-400 rounded w-fit px-1 py-0.5">
+              <FaLinkedin className="text-base"/>
+              <span className="text-sm tracking-wide">Linkedin</span>
+            </div>
+          </a>
+          <a href="https://github.com/fpetranzan" target="_blank">
+            <div className="flex items-center gap-1 border border-slate-400 rounded w-fit px-1 py-0.5">
+              <FaGithub className="text-base"/>
+              <span className="text-sm tracking-wide">Github</span>
+            </div>
+          </a>
+        </div>
+    </>
+  );
+};
+
+export default Social;
