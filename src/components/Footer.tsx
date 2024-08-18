74bc5b375ffaee4af3389a697f2d1663a3344f40
fpetranzan <francesco.petranzan@gmail.com>
2024-08-18 12:05:54 +0200

    added social and logo image to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import Social from "./Social";
+
+
 const Footer = () => {
   
   return (<>
-        <div className="my-20">
+        <Social />
+        <div className="flex items-end gap-4 mb-16">
           <p>fpetranzan.com - My personal Portfolio © 2024 Francesco Petranzan.</p>
+          <Image
+            src="/logo.jpeg"
+            width={50}
+            height={50}
+            alt="fpetranzan.com Logo"
+            className="ml-auto mr-0 border-2 border-slate-400"
+          />
         </div>
     </>
   );
