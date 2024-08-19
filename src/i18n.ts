e490b379f28fcf1dfa873de9a769ac241ac5e852
PETRANZAN Francesco <francesco.petranzan@soprasteria.com>
2024-08-19 20:53:55 +0200

    added i18n configurations

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,16 @@
 import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
 import {locales} from './config';
+import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+
+export type Locale = (typeof locales)[number];
+
+export const { Link, usePathname, useRouter } = createSharedPathnamesNavigation({ locales });
+
+export const localeNames: Record<Locale, string> = {
+  "en": "EN",
+  "it": "IT",
+};
 
 export default getRequestConfig(async ({locale}) => {
   if (!locales.includes(locale as any)) notFound();
