ca6e853f90837b2578e0e5c34850798f5b3c79d9
PETRANZAN Francesco <francesco.petranzan@soprasteria.com>
2024-08-19 21:02:13 +0200

    refactoring and simplify localeSwitch

--- src/components/LocaleSwitch.tsx
+++ src/components/LocaleSwitch.tsx
@@ -1,18 +1,29 @@
-import {useLocale, useTranslations} from 'next-intl';
-import LocaleSwitcherSelect from './LocaleSwitcherSelect';
-import {locales} from '@/config';
+import { Locale, localeNames, usePathname, useRouter } from '@/i18n';
+import { locales } from '@/config';
+import { ChangeEvent } from 'react';
 
-export default function LocaleSwitcher() {
-  const t = useTranslations('LocaleSwitcher');
-  const locale = useLocale();
+export default function LocaleSwitcher({ locale }: { locale: Locale; }) {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  function changeLocale(event: ChangeEvent<HTMLSelectElement>) {
+    const nextLocale = event.target.value as Locale;
+    router.replace(pathname, { locale: nextLocale });
+  }
 
   return (
-    <LocaleSwitcherSelect defaultValue={locale} label={t('label')}>
-      {locales.map((cur) => (
-        <option key={cur} value={cur} className='dark:text-black'>
-          {t('locale', {locale: cur})}
-        </option>
-      ))}
-    </LocaleSwitcherSelect>
+    <div>
+      <select
+        value={locale}
+        onChange={changeLocale}
+        className="bg-transparent"
+      >
+        {locales.map((loc) => (
+          <option key={loc} value={loc} className='dark:text-black'>
+            {localeNames[loc]}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 }
