0c79996415193e13ec423fc674515926f77e8fb3
PETRANZAN Francesco <francesco.petranzan@soprasteria.com>
2024-08-19 21:01:41 +0200

    refactoring headers with new website modifications

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import Link from "next/link";
 import ThemeToggle from "./ThemeToggle";
 import LocaleSwitcher from "./LocaleSwitch";
-import {useTranslations} from 'next-intl';
+import {useLocale, useTranslations} from 'next-intl';
+import { Link, Locale } from "@/i18n";
 
 const Header = () => {
   const t = useTranslations('Menu');
+  const locale = useLocale() as Locale;
   
   return (<>
         <div className="my-20 flex gap-8">
             <Link href={"/"}>{t('home')}</Link>
-            <Link href={"/project"}>{t('project')}</Link>
-            <Link href={"/work"}>{t('work')}</Link>
+            <Link href={"/works"}>{t('works')}</Link>
+            <Link href={"/projects"}>{t('projects')}</Link>
             <ThemeToggle />
-            <LocaleSwitcher />
+            <LocaleSwitcher locale={locale}/>
         </div>
     </>
   );
