bf680045ec0fa4a2e1ec39cce1ae467850c6d294
PETRANZAN Francesco <francesco.petranzan@soprasteria.com>
2024-08-20 19:36:12 +0200

    created manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Francesco Petranzan Portfolio',
+    short_name: 'fpetranzan Portfolio',
+    description: 'Francesco Petranzan Portfolio | All about me, my work experience and my project',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#121212',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
