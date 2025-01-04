c23d9af8fc32760fca6490b634e65df4aa7171e0
fpetranzan <francesco.petranzan@gmail.com>
2025-01-04 16:28:53 +0100

    change margin distance

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
     <div>
       <p className="text-2xl mb-5">{t('title')}</p>
       {t.rich('content', {
-        p: (chunks) => <p className='my-5'>{chunks}</p>
+        p: (chunks) => <p className='my-3'>{chunks}</p>
       })}
     </div>
   </>);
